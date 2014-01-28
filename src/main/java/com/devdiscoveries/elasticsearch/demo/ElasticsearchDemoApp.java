c757066ba11d7e0f8fcf0ed097abd1f6b3ce19fe
Hugo Valk <hugo.valk@gmail.com>
2014-01-28 22:38:38 +0100

    Index a test object in a local running Elasticsearch cluster.

--- src/main/java/com/devdiscoveries/elasticsearch/demo/ElasticsearchDemoApp.java
+++ src/main/java/com/devdiscoveries/elasticsearch/demo/ElasticsearchDemoApp.java
@@ -1,23 +1,48 @@
 package com.devdiscoveries.elasticsearch.demo;
 
+
 import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.devdiscoveries.elasticsearch.demo.domain.Vacancy;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Elasticsearch demo application. 
  */
 public class ElasticsearchDemoApp {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(ElasticsearchDemoApp.class);
+	
     public static void main(String[] args) {
-        System.out.println( "Hello World!" );
+        new ElasticsearchDemoApp().indexDocument();
     }
     
+    private Node node;
+    
     private void indexDocument() {
-    	
+    	final Vacancy vacancy = createTestVacancy();
+    	final Client client = getClient();
+    	String vacancyJson = null;
+    	try {
+    		vacancyJson = new ObjectMapper().writeValueAsString(vacancy);
+        	client.prepareIndex("vacancies", "vacancy")
+        			.setSource(vacancyJson)
+        			.execute().actionGet();
+        	LOG.info("Vacancy has been indexed.");
+    	} catch (final JsonProcessingException jpe) {
+    		LOG.error("Vacancy could not be serialized. ", jpe);
+    	}
+    	client.close();
+    	node.stop();
     }
     
     private Vacancy createTestVacancy() {
@@ -27,18 +52,27 @@ public class ElasticsearchDemoApp {
     	return vacancy;
     }
     
-    private Client getClient() {    
+    private Client getTransportClient() {
     	final Settings settings = ImmutableSettings.settingsBuilder()
-    		.put("node.name", "My beautiful node")
-    		.build();
-    	
-    	final Node node = new NodeBuilder()
-    		.settings(settings)
-    		.clusterName("elasticsearch")
-    		.local(true)
-    		.client(true)
-    		.build();
-    	
+    	        .put("client.transport.sniff", true)
+    	        .put("cluster.name", "elasticsearch-hugo").build();
+    	return new TransportClient(settings)
+    	        .addTransportAddress(new InetSocketTransportAddress("127.0.0.1", 9300));
+    }
+    
+    private Client getClient() {
+    	if (node == null) {
+	    	final Settings settings = ImmutableSettings.settingsBuilder()
+	    		.put("node.name", "My beautiful node")
+	    		.build();
+	    	
+	    	node = new NodeBuilder()
+	    		.settings(settings)
+	    		.clusterName("elasticsearch-hugo")
+	    		//.local(true)
+	    		.client(true)
+	    		.build().start();
+    	}
     	final Client client = node.client();
     	return client;
     }
